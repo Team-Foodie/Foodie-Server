2ec749c3ffd4f7048e96629f7a3dc69d1334e68a
Sangjin <sangjin170202@naver.com>
2022-08-21 15:42:42 +0900

    [FIX] UUID -> Long으로 변경

--- src/main/java/com/foodie/foodie/domain/scrap/domain/repository/ScrapRepository.java
+++ src/main/java/com/foodie/foodie/domain/scrap/domain/repository/ScrapRepository.java
@@ -4,9 +4,7 @@ import com.foodie.foodie.domain.scrap.domain.Scrap;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ScrapRepository extends CrudRepository<Scrap, UUID> {
+public interface ScrapRepository extends CrudRepository<Scrap, Long> {
 
 }
