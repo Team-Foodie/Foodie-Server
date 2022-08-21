ccb0ab537e070a068be80b22dba5bae87b18c252
Sangjin <sangjin170202@naver.com>
2022-08-21 15:17:50 +0900

    [ADD] ResponseItem

---
+++ src/main/java/com/foodie/foodie/global/entity/ResponseItem.java
@@ -0,0 +1,12 @@
+package com.foodie.foodie.global.entity;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@RequiredArgsConstructor
+public class ResponseItem {
+
+    private String pageId;
+
+}
