c68bd6a75f411726a60ae38b7195ea9ef8173d1f
Sangjin <sangjin170202@naver.com>
2022-08-22 21:32:12 +0900

    [ADD] PostRepository

--- src/main/java/com/foodie/foodie/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/foodie/foodie/domain/post/domain/repository/PostRepository.java
@@ -4,7 +4,10 @@ import com.foodie.foodie.domain.post.domain.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
 
+    Optional<Post> findByIdx(Long idx);
 }
