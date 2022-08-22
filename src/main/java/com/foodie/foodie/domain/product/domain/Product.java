2e1394573684a8a9735e6472499b973ed9ce1311
Sangjin <sangjin170202@naver.com>
2022-08-22 21:31:30 +0900

    [FIX] Product -> Jpo 로 변경

--- src/main/java/com/foodie/foodie/domain/product/domain/Product.java
+++ src/main/java/com/foodie/foodie/domain/product/domain/Product.java
@@ -1,30 +1,16 @@
 package com.foodie.foodie.domain.product.domain;
 
 import com.foodie.foodie.global.entity.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-@Table(name = "product")
-public class Product extends BaseEntity {
-
-    @Column(columnDefinition = "VARCHAR(50)", nullable = false)
+@Setter
+public class Product {
     private String name;
-
-    @Column(nullable = false)
     private Integer price;
-
-    @Builder
-    public Product(String name, Integer price) {
-        this.name = name;
-        this.price = price;
-    }
+    private String ProductIntroduction;
 }
